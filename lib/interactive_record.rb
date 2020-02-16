70fcd37f09d7f3032b4bc8c8a482c65b814f2273
Rachel T <rachmaybe@gmail.com>
2020-02-16 03:54:26 +0000

    Automatically backed up by Learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -2,6 +2,12 @@ require_relative "../config/environment.rb"
 require 'active_support/inflector'
 
 class InteractiveRecord
+  def initialize(options={})
+    options.each do |property, value|
+      self.send("#{property}=", value)
+    end
+  end
+  
   def self.table_name
     self.to_s.downcase.pluralize
   end
@@ -18,12 +24,6 @@ class InteractiveRecord
       column_names << column["name"]
     end
     column_names.compact
-  end
-
-  def initialize(options={})
-    options.each do |property, value|
-      self.send("#{property}=", value)
-    end
   end
 
   def table_name_for_insert
